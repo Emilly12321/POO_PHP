728057c4bc45c9480495525547d1aae6206deae8
Emilly12321 <emillysilvaluz2839@gmail.com>
2025-10-27 21:21:24 -0300

    Subindo arquivos

---
+++ Calculadora_Estruturada/processa.php
@@ -0,0 +1,80 @@
+<?php
+
+
+    // Programação estruturada: cada operação é implementada em função separada.
+    function somar($a, $b)
+    {
+        return $a + $b;
+    }
+
+
+    function subtrair($a, $b)
+    {
+            return $a - $b;
+    }
+
+    function multiplicar($a, $b)
+    {
+            return $a * $b;
+    }
+
+    function dividir($a, $b)
+    {
+        if($b == 0){
+            return null;
+        }
+            return $a / $b;
+    }
+
+    //Funcção utilitaria que limpa/normaliza a entrada
+    function parse_num($valor){
+            
+    }
+    
+
+
+
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+
+
+
+
+}
+
+
+
+
+
+
+
+?>
+
+
+
+
+
+
+
+<!-- <!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="assets/css/styles.css">
+
+    <title>Calculadora Estruturada</title>
+</head>
+
+<body>
+    <main>
+    
+
+
+
+    </main>
+
+
+</body>
+
+</html> -->
