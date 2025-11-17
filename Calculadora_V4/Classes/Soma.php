cf82ab3eb501fc6e6353eafed5f39d18b07750f5
Emilly12321 <emillysilvaluz2839@gmail.com>
2025-11-17 17:47:54 -0300

    Add-Calculadora_V4

---
+++ Calculadora_V4/Classes/Soma.php
@@ -0,0 +1,55 @@
+<?php 
+
+final class Soma{
+
+    // Criando os atributos direto na classe para nosso objeto Soma, e serão usadas pelo mesmo.
+    
+    private float $numero1;
+    private float $numero2;
+
+    // Get --> pegar algo ||| Set --> Setar ou definir("armazenar") um valor a algo
+
+
+
+    // Método que permite retornar o valor do $numero1 para quem esteja solicitando, ou seja, o objeto
+    public function getNumero1():float
+    {
+        return $this->numero1;
+    }
+
+    // Método que permite que o valor passado pelo objeto seja recebido no nosso atributo.
+
+    public function setNumero1(float $numero1):void
+    {
+        $this->numero1 = $numero1;
+    }
+    
+
+    // Método que permite retornar o valor do $numero1 para quem esteja solicitando, ou seja, o objeto
+
+    public function getNumero2():float
+    {
+        return $this->numero2;
+    }
+
+
+    // Método que permite que o valor passado pelo objeto seja recebido no nosso atributo.
+
+    public function setNumero2(float $numero2):void
+    {
+        $this->numero2 = $numero2;
+    }
+    
+    // Método Somar entre os atributos...
+
+    public function calcularSoma():float
+    {
+        
+        return $this->numero1 + $this->numero2;
+
+    }
+
+}
+
+
+?>
---
+++ Calculadora_V4/processa.php
@@ -0,0 +1,116 @@
+<?php
+
+/*Importando as Classes para que seja possivel utiliza-las */
+require_once 'Classes\Soma.php';
+require_once 'Classes\TrataeMostra.php';
+
+
+/* Código de controle */
+
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+
+    $valor1 = $_POST['valor1'] ?? " ";
+    $valor2 = $_POST['valor2'] ?? " ";
+    $operacao = $_POST['operacao'] ?? " ";
+
+    $vlr1 = TrataeMostra::convertendoValor($valor1);
+    $vlr2 = TrataeMostra::convertendoValor($valor2);
+
+    $result = null;
+    $error = null;
+
+    /* Criando um objeto a partir da Classe Calculadora */
+    $calculadoraObjeto = new Calculadora();
+
+
+    if ($vlr1 === null || $vlr2 === null) {
+        $error = 'Entrada inválida. Favor preencher corretamente os campos.';
+    } else {
+        switch ($operacao) {
+
+            case 'somar':
+
+                $somaObjeto = new Soma();
+                $somaObjeto->setNumero1($vlr1);
+                $somaObjeto->setNumero2($vlr2);
+                $result = $somaObjeto->calcularSoma();
+
+                break;
+            case 'subtrair':
+
+                $subtrairObjeto = new Subtrair();
+                $subtrairObjeto->setNumero1($vlr1);
+                $subtrairObjeto->setNumero2($vlr2);
+                $result = $subtrairObjeto->calcularSubtrair();
+
+                break;
+
+            case 'multiplicar':
+
+                $multiplicarObjeto = new Multiplicar();
+                $multiplicarObjeto->setNumero1($vlr1);
+                $multiplicarObjeto->setNumero2($vlr2);
+                $result = $multiplicarObjeto->calcularMultiplicar();
+
+                break;
+
+            case 'dividir':
+
+                if ($vlr2 == 0) 
+                {
+
+                    $error = 'Divisão por 0 é inválido, favor informe outro valor';
+
+                } 
+                else 
+                {
+
+                    $dividirObjeto = new Dividir();
+                    $dividirObjeto->setNumero1($vlr1);
+                    $dividirObjeto->setNumero2($vlr2);
+                    $result = $dividirObjeto->calcularDividir();
+
+                }
+
+                break;
+
+            default:
+                $error = 'Operação inválida';
+        }
+    }
+
+}
+
+?>
+
+
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="assets/css/styles.css">
+
+    <title>Calculadora_POOV2</title>
+</head>
+
+<body>
+    <main class="container">
+
+        <?php
+
+        
+            TrataeMostra::exibirResultado($error,$operacao,$vlr1,$vlr2,$result);
+
+        ?>
+
+    </main>
+
+
+</body>
+
+</html>
+
+
+
