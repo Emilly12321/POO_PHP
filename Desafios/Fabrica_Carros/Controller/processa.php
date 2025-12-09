74b92de23abec9f64b1e696dde1532059dec37c3
Emilly12321 <emillysilvaluz2839@gmail.com>
2025-12-09 14:04:14 -0300

    refatorar método venderCarro()

--- Desafios/Fabrica_Carros/Controller/processa.php
+++ Desafios/Fabrica_Carros/Controller/processa.php
@@ -40,12 +40,13 @@ if($_SERVER['REQUEST_METHOD']==="POST"){
             if(isset($_SESSION['fabrica'])){
                $fabrica = unserialize($_SESSION['fabrica']);
             }else{
+                
                 $fabrica = new Fabrica();
             }
 
             $fabrica->fabricarCarros($modelo,$cor,$qtdeFabricar);
 
-            $_SESSION['fabrica'] = serialize($fabrica);
+            $_SESSION['fabrica']= serialize($fabrica);
 
             echo "
             <form>
@@ -59,13 +60,71 @@ if($_SERVER['REQUEST_METHOD']==="POST"){
 
 
         break;
+        case "venda":
+            echo '
+            
+            <h1>Informe o Modelo e Cor que deseja vender</h1>
+            
+            <form action="processa.php" method="POST">
+            
+            <input type="hidden" name="acao" value="venderCarro";
+            
+            <label>Modelo: </label>
+            <input type="text" name="modelo">
+            <label>Cor: </label>
+            <input type="text" name="cor">
+            <button type="submit">Avançar</button>
+            
+            </form>';
+            break;
+        case "venderCarro":
+
+             if(!isset($_SESSION['fabrica'])){
+                echo"<p>Não há carros cadastrados com essas informações!</p><br>
+                <a href='..\View\index.html'>Voltar ao menu</a>";
+                break;
+            }
+
+            $fabrica = unserialize($_SESSION['fabrica']);
+
+            $modelo = $_POST['modelo'] ?? "";
+            $cor = $_POST['cor'] ?? "";
+
+            $validador = $fabrica->venderCarros($modelo,$cor);
+
+            if($validador){
+                
+                echo "
+                <h3>Vendidos com sucesso!!</h3>
+                <form>
+                
+                <label>Modelo:</label>
+                <input type='hidden' name='modelo' value='{$modelo}'>
+                <label>Cor:</label>
+                <input type='hidden' name='cor' value='$cor'>
+                
+                
+                </form>";
+
+            }else{
+                echo"Não há carros cadastrados com essas informações!!";
+            }
+            
+            $_SESSION['fabrica']= serialize($fabrica);
+
+            echo "<a href='..\View\index.html'>Voltar ao menu</a>";
+
+        break;
         case "info":
             if(!isset($_SESSION['fabrica'])){
-                echo"<p>Não há carros cadastrados!</p>";
+                echo"<p>Não há carros cadastrados!</p><br>
+                <a href='..\View\index.html'>Voltar ao menu</a>";
+                break;
             }
             $fabrica = unserialize($_SESSION['fabrica']);
             echo "Carros fabricados: <br> ";
             $fabrica->mostrarCarros();
+            echo "<a href='..\View\index.html'>Voltar ao menu</a>";
             break;
     }
 
--- Desafios/Fabrica_Carros/Models/Fabrica.php
+++ Desafios/Fabrica_Carros/Models/Fabrica.php
@@ -4,7 +4,7 @@ require_once 'Carro.php';
 class Fabrica{  
     private array $guardarCarros = [];
 
-    public function fabricarCarros(string $modelo, string $cor , int $qtdade){
+    public function fabricarCarros(string $modelo, string $cor , int $qtdade):void{
        
         for($i = 1 ; $i <= $qtdade ; $i++){
             $carro = new Carro();
@@ -12,6 +12,28 @@ class Fabrica{
             $carro->setCor($cor);
             $this->guardarCarros[] = $carro;
         }
+    }
+
+    public function venderCarros(string $modelo, string $cor){
+
+        print_r($this->guardarCarros);
+
+    
+        foreach($this->guardarCarros as $i => $guardarCarros){
+
+            if( $guardarCarros->getModelo() === $modelo &&  $guardarCarros->getCor() === $cor){
+
+                unset($this->guardarCarros[$i]);
+              
+                $this->guardarCarros = array_values($this->guardarCarros);
+               
+                return true;
+                
+            }
+
+        }
+        return false;
+
     }
 
 
@@ -23,6 +45,7 @@ class Fabrica{
         }
 
     }
+
 }
 
 
