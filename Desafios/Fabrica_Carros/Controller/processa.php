832a6234b6fad9a6fd17cda0126a3ea55df94ef3
Emilly12321 <emillysilvaluz2839@gmail.com>
2025-12-11 21:43:06 -0300

    add

--- Desafios/Fabrica_Carros/Controller/processa.php
+++ Desafios/Fabrica_Carros/Controller/processa.php
@@ -14,13 +14,10 @@ if($_SERVER['REQUEST_METHOD']==="POST"){
             <section>
             
             <form action="processa.php" method="POST">
-            <input type="hidden" name="acao" value="salvarCarro">
+            <input type="hidden" name="acao" value="criandoCarro">
             
-            <label>Modelo:</label>
-            <input type="text" name="modelo">
-            <label>Cor:</label>
-            <input type="text" name="cor">
-            <label>Quantidade a ser Fabricada:</label>
+            
+            <label>Quantidade de carros a serem Fabricados:</label>
             <input type="number" min="1" name="qtdeFabricar">
             <button type="submit">Confirmar</button>
             
@@ -31,10 +28,41 @@ if($_SERVER['REQUEST_METHOD']==="POST"){
             
             
             ';
+            echo "
+                <a href='..\View\index.html'>Voltar ao menu</a>";
+            break;
+        case"criandoCarro":
+            
+            $qtdeFabricar = $_POST['qtdeFabricar']??"";
+
+            echo "
+            <section>
+
+            <form action='processa.php' method='POST'>
+            <input type='hidden' name='acao' value='salvarCarro'>
+            <input type ='hidden' name='qtdeFabricar' value='{$qtdeFabricar}'>";
+
+            for($i = 1 ; $i <= $qtdeFabricar ; $i++){
+
+                echo"<br>
+                <label>Modelo:</label>
+                <input type='text' name='modelo_{$i}'><br>
+                <label>Cor:</label>
+                <input type='text' name='cor_{$i}'>
+                <br><br>
+                ";
+            }
+                
+                echo'<button type="submit">Confirmar</button>
+                </form></section>';
+                echo "
+                <a href='..\View\index.html'>Voltar ao menu</a>";
+                
+            
+
             break;
         case "salvarCarro":
-            $modelo = $_POST['modelo'] ?? "";
-            $cor = $_POST['cor'] ?? "";
+
             $qtdeFabricar = $_POST['qtdeFabricar']??"";
           
             if(isset($_SESSION['fabrica'])){
@@ -44,17 +72,21 @@ if($_SERVER['REQUEST_METHOD']==="POST"){
                 $fabrica = new Fabrica();
             }
 
-            $fabrica->fabricarCarros($modelo,$cor,$qtdeFabricar);
+            $veiculos = [];
+
+            for($i = 1 ;$i <= $qtdeFabricar ; $i++){
+
+                $carro = new Carro();
+                $carro->setModelo($_POST["modelo_{$i}"]??"");
+                $carro->setCor($_POST["cor_{$i}"]??"");
+                $veiculos[] = $carro;
+            }
+            $fabrica->fabricarCarros($veiculos);
 
             $_SESSION['fabrica']= serialize($fabrica);
 
             echo "
-            <form>
-            
-            <label>Modelo: {$modelo}</label> <br>
-            <label>Cor: {$cor}</label> <br>
-            <label>Quantidade que foram cadastrados: {$qtdeFabricar}</label><br>
-            </form> <br>
+            <h3>Sucesso na Fabricação</h3>
             <a href='..\View\index.html'>Voltar ao menu</a>
             ";
 
@@ -76,9 +108,17 @@ if($_SERVER['REQUEST_METHOD']==="POST"){
             <button type="submit">Avançar</button>
             
             </form>';
+            echo "
+                <a href='..\View\index.html'>Voltar ao menu</a>";
             break;
         case "venderCarro":
-            
+          
+            if(!isset($_SESSION['fabrica'])){
+                echo"<h1>Não há carros fabricados!!</h1>";
+                echo "<a href='..\View\index.html'>Voltar ao menu</a>";
+                break;
+            }
+
             $modelo = $_POST['modelo'] ?? "";
             $cor = $_POST['cor'] ?? "";
 
@@ -90,14 +130,8 @@ if($_SERVER['REQUEST_METHOD']==="POST"){
             if($validador){
                 
                 echo "
-                <h3>Carros vendidos com sucesso!!</h3>
-
-                <form>
-                
-                <label>Modelo: {$modelo}</label><br>
-                <label>Cor: {$cor}</label>                
-                
-                </form>";
+                <h3>Carro vendido com sucesso!!</h3>
+                <p>Modelo: {$modelo} e Cor: {$cor}</p>";
 
             }else{
                 echo"Não há carros cadastrados com essas informações!!";
@@ -117,8 +151,22 @@ if($_SERVER['REQUEST_METHOD']==="POST"){
             $fabrica = unserialize($_SESSION['fabrica']);
             echo "Carros fabricados: <br> ";
             $fabrica->mostrarCarros();
+            echo"<form action='processa.php' method='POST'>
+            
+            <input type='hidden'name='acao' value='finalizar_secao'>
+            <button>Fechar a Fábrica</button>
+            
+            </form>";
             echo "<a href='..\View\index.html'>Voltar ao menu</a>";
             break;
+        case "finalizar_secao":
+            session_destroy();
+            echo"<h1>Fábrica Fechada, carros foram destruídos...</h1>";
+            echo "<a href='..\View\index.html'>Voltar ao menu</a>";
+            break;
+        default:
+            echo"<h1></";
+        break;
     }
 
 
--- Desafios/Fabrica_Carros/Models/Fabrica.php
+++ Desafios/Fabrica_Carros/Models/Fabrica.php
@@ -4,51 +4,36 @@ require_once 'Carro.php';
 class Fabrica{  
     private array $guardarCarros = [];
 
-    public function fabricarCarros(string $modelo, string $cor , int $qtdade):void{
-       
-        for($i = 1 ; $i <= $qtdade ; $i++){
-            $carro = new Carro();
-            $carro->setModelo($modelo);
-            $carro->setCor($cor);
-            $this->guardarCarros[] = $carro;
+    public function fabricarCarros(array $carro):void{
+        foreach($carro as $carro){
+            $this->guardarCarros[$i] = $carro;
         }
     }
 
     public function venderCarros(string $modelo, string $cor):bool{
-
-        $validador = 0;
     
         foreach($this->guardarCarros as $i => $guardarCarros){
 
             if( $guardarCarros->getModelo() === $modelo &&  $guardarCarros->getCor() === $cor){
 
                 unset($this->guardarCarros[$i]);
-                $validador++;
+                return true;
 
             }
             
         }
-
-        if($validador>0){
-
-            return true;
-
-        }else{
-
-            return false;
-
-        }
+        return false;
         
     }
 
 
 
-    public function mostrarCarros():string{
+    public function mostrarCarros():void{
 
         $this->guardarCarros = array_values($this->guardarCarros);
 
         foreach($this->guardarCarros as $i => $carro){
-            echo "<p>".($i+1)."° Carro, Modelo:".$carro->getModelo()." Cor:".$carro->getCor()."</p><br>";
+            echo "<p>".($i+1)."° Carro: <br> <strong>Modelo:</strong> ". $carro->getModelo()." ||  <strong>Cor:</strong> ".$carro->getCor()."</p><br><br>";
         }
 
     }
